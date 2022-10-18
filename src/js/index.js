0cca92ad261280600944afc971db677ca470231c
lassev05 <vestergaardlasse2@gmail.com>
2022-10-18 19:36:48 +0200

    Add projects

--- src/js/index.js
+++ src/js/index.js
@@ -15,3 +15,32 @@ fetch("/data.json")
   .catch((e) => {
     console.error(e.message);
   });
+
+// Projects
+fetch("/projects.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const CardList = document.getElementById("cardList");
+
+    CardList.innerHTML = data.map((p) => {
+      return `<div class="card">
+      <img
+        src="${p.image}"
+        class="card-img-top"
+        alt="${p.id}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${p.name}</h5>
+        <p class="card-text">
+         ${p.description}
+        </p>
+        <a href="${p.link}" target="_blank" class="card-text">
+          Besøg siden
+        </a>
+      </div>
+    </div>`;
+    });
+  })
+  .catch((e) => {
+    console.error(e.message);
+  });
